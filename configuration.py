5260fc4973db1434896cc5da6564b1a0c582ff2d
Darayus Nanavati <darayus.contact@gmail.com>
2013-07-11 21:43:34 +1000

    New settings format (As a dictionary for custom user email servers)

--- configuration.py
+++ configuration.py
@@ -1,6 +1,19 @@
-# configuration.py:
+"""
+Setting details:
 
-hostname = '8.8.8.8'  #your servers hostname or IP address
-recipient_email = 'recipient@yourdomain.com' #receiving email address (to)
-monitor_email = 'monitor@yourdomain.com' #monitor email address (from) - must be a Gmail Address
-monitor_pwd = 'monitor email password' #monitor email password
+    hostname            - The hostname of the server to check (For example, your servers hostname or IP address)
+    recipient_email     - The email where downtime alerts will be sent to
+    monitor_email       - The email address to be sent from
+    monitor_password    - The password of the email address to be sent from
+    monitor_server      - The email server to send the emails from
+    monitor_server_port - The email server smtp port
+
+"""
+settings = {
+    "hostname": '8.8.8.8',
+    "recipient_email": 'recipient@yourdomain.com',
+    "monitor_email": 'monitor@yourdomain.com',
+    "monitor_password": 'monitor email password',
+    "monitor_server": 'smtp.gmail.com',
+    "monitor_server_port": 587,
+}
