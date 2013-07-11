4cffc649c111cc47a4d9108678694f53fefe87cc
brendancarlson <brendan@bcarlsonmedia.com>
2013-07-10 22:05:52 -0700

    Update myconfig.py

--- myconfig.py
+++ myconfig.py
@@ -2,5 +2,5 @@
 
 hostname = '8.8.8.8'  #your servers hostname or IP address
 recipient_email = 'recipient@yourdomain.com' #receiving email address (to)
-monitor_email = 'monitor@yourdomain.com' #monitor email address (from)
+monitor_email = 'monitor@yourdomain.com' #monitor email address (from) - must be a Gmail Address
 monitor_pwd = 'monitor email password' #monitor email password
